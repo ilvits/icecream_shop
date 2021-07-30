ac33fff26e20b9a04e377b39eed2af970ad92917
ilvits <tsoyilya@gmail.com>
2021-07-30 11:46:14 +0300

    go on

--- orders/models.py
+++ orders/models.py
@@ -6,12 +6,9 @@ class Order(models.Model):
     first_name = models.CharField(max_length=50, verbose_name='Имя')
     last_name = models.CharField(max_length=50, verbose_name='Фамилия')
     email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False, verbose_name='Оплачено')
 
     class Meta:
         ordering = ('-created',)
