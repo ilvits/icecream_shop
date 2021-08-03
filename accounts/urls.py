3de45b661a64150f7ef4ed16dc4dfb47b06d1fbd
ilvits <tsoyilya@gmail.com>
2021-08-03 19:36:51 +0000

    some UI fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf.urls import include
-
+from .views import SignUpView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
