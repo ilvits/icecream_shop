95a862a1004a81b4af1a6168a978245a12c256c8
ilvits <tsoyilya@gmail.com>
2021-08-09 20:37:21 +0300

    bug fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,9 @@ from .views import SignUpView
 from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    
     path('signup/', SignUpView.as_view(), name='signup'),
     url(r'^profile/', views.user_profile, name='user_profile'),
 	url(r'^order/(?P<order_id>[-\w]+)/$', views.order_detail, name='order_detail'),
+    path('', include('django.contrib.auth.urls')),
 ]
