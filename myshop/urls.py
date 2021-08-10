2e0022e7c047891702e83993325c387358368dc0
ilvits <tsoyilya@gmail.com>
2021-08-10 10:55:28 +0300

    reset password tuning

--- myshop/urls.py
+++ myshop/urls.py
@@ -8,7 +8,7 @@ from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    url(r'^favicon\.ico$', RedirectView.as_view(url='favicon.ico'), name='favicon'),
+    path('favicon.ico', RedirectView.as_view(url='media/favicon.ico')),
     path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
