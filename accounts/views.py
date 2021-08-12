e4b4ebd1870d14f490bbea5a3522658a1ed9e08c
ilvits <tsoyilya@gmail.com>
2021-08-12 20:45:07 +0300

    edit profile image errors

--- accounts/views.py
+++ accounts/views.py
@@ -109,7 +109,7 @@ def password_reset_request(request):
 def edit_profile(request):
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        profile_form = ProfileForm(request.POST, request.FILES or None, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
