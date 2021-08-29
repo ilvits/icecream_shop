8d0258661656a9852083e9738170e31b6f1adef1
ILViTs <tsoyilya@gmail.com>
2021-08-29 13:28:47 +0300

    view list refactor from def to class

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
+from .models import Product
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
-    url(r'^(?P<category_slug>[-\w]+)/$',
-        views.product_list,
-        name='product_list_by_category'),
+    path('', views.ProductView.as_view(),  name="product_list"),
+    path('<category_slug>', views.ProductView.as_view(),  name="product_list_by_category"),
+    # url(r'^$', views.product_list, name='product_list'),
+    # url(r'^(?P<category_slug>[-\w]+)/$',
+    #     views.product_list,
+    #     name='product_list_by_category'),
     # url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
     #     views.product_detail,
     #     name='product_detail'),
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.generic.list import ListView
+
 from .tasks import send_contact_form
 
 from .models import Category, Product
@@ -13,10 +16,34 @@ def homepage(request):
     return render(request, 'shop/home.html')
 
 
-# def test(request):
-#     return render(request, 'shop/test.html')
+@method_decorator(login_required, name='dispatch')
+class ProductView(ListView):
+    model = Product
+    template_name = 'shop/product/list.html'
+    context_object_name = 'products'
+    paginate_by = 2
 
-@login_required
+    def get_queryset(self):
+        if self.kwargs:
+            category = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+            products = Product.objects.filter(category=category)
+        else:
+            products = Product.objects.all()
+        return products
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        category = None
+        if self.kwargs:
+            category = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        # Add in the product
+        context['categories'] = categories
+        context['category'] = category
+        return context
+
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -25,7 +52,7 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request,
-                  'shop/product/list.html',
+                  'shop/product/listw.html',
                   {'category': category,
                    'categories': categories,
                    'products': products}
