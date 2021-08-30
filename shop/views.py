82d436d15fe5e0693da6d80e00772d0c252b0342
ILViTs <tsoyilya@gmail.com>
2021-08-30 12:43:15 +0300

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -44,20 +44,20 @@ class ProductView(ListView):
 
         return context
 
-
-def product_list(request, category_slug=None):
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request,
-                  'shop/product/listw.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products}
-                  )
+#
+# def product_list(request, category_slug=None):
+#     category = None
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(available=True)
+#     if category_slug:
+#         category = get_object_or_404(Category, slug=category_slug)
+#         products = products.filter(category=category)
+#     return render(request,
+#                   'shop/product/listw.html',
+#                   {'category': category,
+#                    'categories': categories,
+#                    'products': products}
+#                   )
 
 
 # @login_required
